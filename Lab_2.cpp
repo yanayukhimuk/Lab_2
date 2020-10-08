fda01a4414b6b7cb9ad104e46ca023cec05616b6
ASUS <ASUS@DESKTOP-R0579L9>
2020-10-08 19:04:35 +0300

    Изменила код

---
+++ Lab_2.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <Windows.h>
+using namespace std;
+void outp(char* str) {
+	printf("%s\n%d - размер\n", str, strlen(str));
+}
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	char str[256];
+	cin.getline(str, 256);
+	outp(str);
+	int l = strlen(str);
+	int count = 0;
+	for (int i = 0; i < l; i++)
+		if (str[i] == str[0])
+			count++; // count = count + 1; <=> count +=1;
+		else
+			if (str[i] == '.')
+				str[i] = str[0];
+	printf("%d - кол-во\nНовую строку после замены '.' на %c\n%s - новая строка\n", count, str[0], str);
+	system("pause");
+	return 0;
+}
